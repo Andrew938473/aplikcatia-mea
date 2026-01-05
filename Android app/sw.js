d10f50a30e998907798fb3e097a8bb6a6aabd1ca
Andrei <151470154+Andrew938473@users.noreply.github.com>
2026-01-06 01:01:51 +0200

    Add files via upload

---
+++ Android app/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        'index.html'
+      ]);
+    })
+  );
+});
